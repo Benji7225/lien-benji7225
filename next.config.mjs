1ab605521373a21b1697f92ad4b9dead602bdb30
Benji7225 <benjamin.ezidu77@gmail.com>
2026-01-09 12:09:53 +0100

    Mes modifications

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["lucide-react"],
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pinimg.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
